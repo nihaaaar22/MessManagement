55af6eac18c5f086fbd0c85e1a3250e6394b3523
Sahil <omanwarsahil@gmail.com>
2024-06-22 12:13:53 +0530

    All models defined

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,2 +1,35 @@
 //schema contains username, password(encrypted), phno, address and role
 // t
+
+const mongoose=require("mongoose");
+
+const  userSchema=new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            maxLength:30
+        },
+        password:{
+            type:String,
+            required:true,
+            maxLength:30
+        },
+        phoneNumber:{
+           type:Number,
+           required:true,
+           maxLength:10
+        },
+        address:{
+            type:String,
+            required:true,
+            maxlength:50
+        }
+    }
+)
+
+
+module.exports=mongoose.model("User",userSchema);
+
+
+
