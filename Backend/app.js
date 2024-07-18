fa71d10d1caeaea8a7b705433e831005335ed54e
Sahil <omanwarsahil@gmail.com>
2024-07-18 12:41:45 +0530

    Addded dotenv file and changed routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-const cors = require('cors')
+const cors = require('cors');
+const connectDb = require('./config/db');
+const dotenv=require('dotenv');
+const authMiddleware=require('./middlewares/authMiddleware')
+const customerRoutes=require('./routes/customerRoutes');
+const adminRoutes=require('./routes/adminRoutes')
+const errorMiddleware=require('./middlewares/errorMiddleware')
+
+
+dotenv.config();
+
+const PORT=process.env.PORT || 3000 
 
 // const customerRoutes = require('./routes/customerRoutes');
 // const adminRoutes = require('./routes/adminRoutes');
@@ -12,6 +23,7 @@ const app = express();
 // app.use(bodyParser.urlencoded({extended:true}))
 // app.use(bodyParser.json());
 app.use(cors());
+connectDb();
 
 // Middleware
 app.use(express.json());
@@ -19,9 +31,13 @@ app.use('/auth', authRoutes);
 app.use('/hello',(req,res)=>{
     res.send("hello world")
 })
-// app.use('/customer', authMiddleware, customerRoutes);
-// app.use('/admin', authMiddleware, adminRoutes);
-// app.use(errorMiddleware);
+app.use('/customer', authMiddleware, customerRoutes);
+app.use('/admin', authMiddleware, adminRoutes);
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
 // MongoDB connection
 // mongoose.connect('mongodb://localhost:27017/hotelApp', { useNewUrlParser: true, useUnifiedTopology: true })
