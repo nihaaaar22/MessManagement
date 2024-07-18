4317e87789bb09b127280f8f9bbacdbec8690ac4
Sahil <omanwarsahil@gmail.com>
2024-07-18 12:36:48 +0530

    Wrote code for error Middleware

--- Backend/middlewares/errorMiddleware.js
+++ Backend/middlewares/errorMiddleware.js
@@ -1,1 +1,10 @@
-//routes here in case of invalid request
+
+const errorMiddleware = (err, req, res, next) => {
+    console.error(err.stack);
+  
+    res.status(err.status || 500).json({
+      message: err.message || 'Internal Server Error',
+    });
+  };
+  
+  module.exports = errorMiddleware;
